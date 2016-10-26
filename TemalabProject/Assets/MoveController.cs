76be25f94be67fa1eff5f38571233392be91fde3
Oliver <dvdrip.xvid.avi@gmail.com>
2016-10-26 09:51:23 +0200

    animation and navigation

---
+++ TemalabProject/Assets/MoveController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+    public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseUp()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (GetComponent<TerrainCollider>().Raycast(ray, out hit, Mathf.Infinity))
+        {
+            agent.SetDestination(hit.point);
+        }
+    }
+
+}
