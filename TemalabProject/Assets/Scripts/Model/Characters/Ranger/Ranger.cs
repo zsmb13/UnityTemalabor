d0d8d51b69dadeaec162dbce2c11e07d4af75a13
Oliver <dvdrip.xvid.avi@gmail.com>
2016-11-25 02:08:46 +0100

    Ranger implementáció[not ready] + Scene-re pakolása

---
+++ TemalabProject/Assets/Scripts/Model/Characters/Ranger/Ranger.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Model.Skills;
+using System;
+
+namespace Assets.Scripts.Model.Characters {
+
+    public class Ranger : Character {
+
+        public void Awake() {
+            ConstStats constStats = new ConstStats();
+
+            constStats.Name = "Julia";
+            constStats.CharacterType = "Ranger";
+            constStats.DodgeChance = 10;
+            constStats.MagicResist = 0;
+            constStats.PhysicalResist = 0;
+            constStats.TotalHealth = 200;
+            constStats.TotalMovement = 5;
+
+            List<Skill> skills = new List<Skill>();
+            skills.Add(new Walk());
+            skills.Add(new BasicAttack_Ranger());
+            // TODO: implement strafe
+            skills.Add(new Walk());
+
+            Init(constStats, skills);
+        }
+
+        protected override float GetDamagedDelay() {
+            return 2;
+        }
+
+        protected override float GetDeathDelay() {
+            return 2;
+        }
+
+        protected override float GetDodgeDelay() {
+            return 2;
+        }
+    }
+}
---
+++ TemalabProject/Assets/Scripts/Model/Characters/Ranger/Skills/BasicAttack_Ranger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Scripts.Model;
+
+namespace Assets.Scripts.Model.Skills {
+
+    public class BasicAttack_Ranger : EnemySkill {
+
+        private static readonly int cooldown = 2;
+        private static readonly string name = "Basic attack";
+        private static readonly string description = "Deal 70 damage to a target unit.";
+
+        private static readonly int damage = 70;
+        private static readonly float animationDelay = 1.08f;
+
+        private static readonly float minDistance = 10;
+
+        public BasicAttack_Ranger() : base(name, description, cooldown) { }
+
+        protected override void OnExecute(Character source, object target) {
+            Character enemy = target as Character;
+
+            Result result = new Result(0, false);
+            if (enemy.TryPhysicalDodge()) {
+                enemy.OnDodge(0.5f);
+            } else {
+                result = enemy.OnPiercingDamage(damage, animationDelay);
+            }
+
+            source.OnAttack(enemy, "Attack");
+
+            source.AfterAttack(enemy, result);
+            enemy.AfterDefense(source, result);
+
+            source.TurnStats.ActionPoints--;
+        }
+
+        public override float GetRange(Character source) {
+            return 200;
+        }
+
+        public override bool IsAvailable(TurnStats stats) {
+            return stats.ActionPoints > 0;
+        }
+
+        protected override bool IsValidTarget(Character source, object target) {
+            if(target is Character) {
+                Character targetCharacter = target as Character;
+                return (source.gameObject.transform.position - targetCharacter.gameObject.transform.position).magnitude < minDistance;
+            }
+            return false;
+        }
+
+    }
+
+}
